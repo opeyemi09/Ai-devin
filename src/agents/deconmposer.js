179a58c86fab6f019a8c6f773fa7b992ad97ace4
opeyemi09 <akinboroadesina@gmail.com>
2026-08-05 12:58:51 +0100

    Delete src/agents/deconmposer.js

--- src/agents/deconmposer.js
+++
@@ -1,8 +0,0 @@
-class Decomposer {
-  async run(task = {}) {
-    // placeholder decomposition
-    return { success: true, output: ["subtask-1", "subtask-2"] };
-  }
-}
-
-module.exports = Decomposer;
