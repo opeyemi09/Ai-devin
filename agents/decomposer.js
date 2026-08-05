408d990835cbd573753f9312a1d4bb2aa00e8b7a
opeyemi09 <akinboroadesina@gmail.com>
2026-08-05 12:00:57 +0100

    Add Decomposer class with a placeholder run method

---
+++ agents/decomposer.js
@@ -0,0 +1,8 @@
+class Decomposer {
+  async run(task = {}) {
+    // placeholder decomposition
+    return { success: true, output: ["subtask-1", "subtask-2"] };
+  }
+}
+
+module.exports = Decomposer;
